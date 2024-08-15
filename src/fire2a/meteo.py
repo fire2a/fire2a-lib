2ff9d2f1d0338456df7512d69d66d83ad012988b
fdobad <8339628-fdobad@users.noreply.gitlab.com>
2024-08-15 00:45:43 -0400

    fix

--- src/fire2a/meteo.py
+++ src/fire2a/meteo.py
@@ -5,15 +5,46 @@ Some functions related to kitral weather scenario creation.
 __author__ = "Caro"
 __revision__ = "$Format:%H$"
 
+from datetime import datetime, timedelta
 from pathlib import Path
+
 import numpy as np
 import pandas as pd
-import random
 
-
+# debug aqui = Path()
 aqui = Path(__file__).parent
 # Ruta a los datos de estaciones
-ruta_data = aqui / "./DB_DMC/"
+ruta_data = aqui / "DB_DMC"
+
+
+def file_name(i, numsims):
+    if numsims > 1:
+        return f"Weather{i+1}.csv"
+    return "Weather.csv"
+
+
+def scenario_name(i, numsims):
+    if numsims > 1:
+        return f"DMC_{i+1}"
+    return "DMC"
+
+
+def distancia(fila, y_i, x_i):
+    if y_i == fila["lat"] and x_i == fila["lon"]:
+        return 0
+    return np.sqrt((fila["lat"] - x_i) ** 2 + (fila["lon"] - y_i) ** 2)
+
+
+def meteo_to_c2f(alfa):
+    if alfa >= 0 and alfa < 180:
+        return round(alfa + 180, 2)
+    elif alfa >= 180 and alfa <= 360:
+        return round(alfa - 180, 2)
+    return np.nan
+
+
+def barlo_sota(a):
+    return round((a + 180) % 360, 2)
 
 
 def generate(x, y, start_datetime, rowres, numrows, numsims, outdir):
@@ -21,7 +52,7 @@ def generate(x, y, start_datetime, rowres, numrows, numsims, outdir):
     Args:
         x (float): x-coordinate of the ignition point, EPSG 4326
         y (float): y-coordinate of the ignition point, EPSG 4326
-        starttime (starttime): start datetime of the weather scenario label (not implemented yet)
+        start_datetime (datetime): starting time of the weather scenario
         rowres (int): time resolution in minutes (not implemented yet)
         numrows (int): number of hours in the weather scenario
         numsims (int): number of weather scenarios
@@ -30,73 +61,66 @@ def generate(x, y, start_datetime, rowres, numrows, numsims, outdir):
         retval (int): 0 if successful, 1 otherwise, 2...
         outdict (dict): output dictionary at least 'filelist': list of filenames created
     """
+    filelist = []
     try:
 
         if not outdir.is_dir():
             outdir.mkdir()
-
-        def file_name(i, numsims):
-            if numsims > 1:
-                return f"Weather{i+1}.csv"
-            return "Weather.csv"
-
-        def scenario_name(i, numsims):
-            if numsims > 1:
-                return f"DMC_{i+1}"
-            return "DMC"
-
-        def distancia(fila, y_i, x_i):
-            if y_i == fila["lat"] and x_i == fila["lon"]:
-                return 0
-            return np.sqrt((fila["lat"] - x_i) ** 2 + (fila["lon"] - y_i) ** 2)
-
-        def meteo_to_c2f(alfa):
-            if alfa >= 0 and alfa < 180:
-                return round(alfa + 180,2)
-            elif alfa >= 180 and alfa <= 360:
-                return round(alfa - 180,2)
-            return np.nan
 
         dn = 3
         list_stn = pd.read_csv(ruta_data / "Estaciones.csv")
         list_stn["Distancia"] = list_stn.apply(distancia, args=(y, x), axis=1)  # calculo distancia
         stn = list_stn.sort_values(by=["Distancia"]).head(dn)["nombre"].tolist()
 
-        meteos= pd.DataFrame()
-
+        meteos = pd.DataFrame()
         for st in stn:
-            df=pd.read_csv(ruta_data /  f"{st}.csv", sep=",")
-            df["station"]=st
-            meteos=pd.concat([meteos, df], ignore_index=True)
+            df = pd.read_csv(ruta_data / f"{st}.csv", sep=",")
+            df["station"] = st
+            meteos = pd.concat([meteos, df], ignore_index=True)
+        meteos["datetime"] = pd.to_datetime(meteos["datetime"], errors="coerce")
+        # available days by stations
+        days = meteos.groupby(meteos.datetime.dt.date).first()["station"]
 
-
-        filelist = []
         for i in range(numsims):
-            scenario = [scenario_name(i, numsims)] * numrows
-
-            selected_station = meteos[ meteos['station'] == np.random.choice(stn) ]
-
-            if len(selected_station["TMP"])<numrows:
-                return 1, {"filelist": [], "exception": "Not enough data"}
-
-            m = random.randint(0,len(selected_station["TMP"]) - numrows)
-
-            WD=[]
-            for x in selected_station["WD"].iloc[m:m+numrows]:
-                WD.append((meteo_to_c2f(x)))
-
-            dt = selected_station["datetime"].iloc[m : m + numrows]
-            WS = selected_station["WS"].iloc[m : m + numrows]
-            TMP = selected_station["TMP"].iloc[m : m + numrows]
-            RH = selected_station["RH"].iloc[m : m + numrows].tolist()
-
-            df = pd.DataFrame(
-            np.vstack((scenario, dt, WS, WD, TMP, RH)).T,
-                columns=["Scenario", "datetime", "WS", "WD", "TMP", "RH"],
-            )
+            # draw station and day
+            cd = 0
+            ch = 0
+            while True:
+                station = np.random.choice(stn)
+                chosen_days = days[days == station]
+                if chosen_days.empty:
+                    if cd > 10:
+                        # print("Not enough data days", cd, ch)
+                        return 1, {"filelist": [], "exception": "No data in closest stations"}
+                    cd += 1
+                    continue
+                day = np.random.choice(chosen_days.index)
+                start = datetime.combine(day - timedelta(days=ch), start_datetime.time())
+                chosen_meteo = meteos[(meteos["datetime"] >= start) & (meteos["station"] == station)]
+                if len(chosen_meteo) < numrows:
+                    if ch > len(meteos):
+                        # print("Not enough data hours", cd, ch)
+                        return 1, {"filelist": [], "exception": "Not enough data"}
+                    ch += 1
+                    continue
+                break
+            # take rows
+            chosen_meteo = chosen_meteo.head(numrows)
+            # drop station
+            chosen_meteo = chosen_meteo.drop(columns=["station"])
+            # wind direction
+            chosen_meteo.loc[:, "WD"] = chosen_meteo["WD"].apply(barlo_sota)
+            # scenario name
+            chosen_meteo.loc[:, "Scenario"] = scenario_name(i, numsims)
+            # datetime format
+            chosen_meteo.loc[:, "datetime"] = chosen_meteo["datetime"].dt.strftime("%Y-%m-%dT%H:%M:%S")
+            # reorder
+            chosen_meteo = chosen_meteo[["Scenario", "datetime", "WS", "WD", "TMP", "RH"]]
+            # write
+            # print("head", chosen_meteo.head())
             tmpfile = outdir / file_name(i, numsims)
             filelist += [tmpfile.name]
-            df.to_csv(tmpfile, header=True, index=False)
+            chosen_meteo.to_csv(tmpfile, header=True, index=False)
         return 0, {"filelist": filelist}
 
     except Exception as e:
--- tests/test_meteo.py
+++ tests/test_meteo.py
@@ -13,25 +13,27 @@ x = -36
 y = -72
 
 
-def test_create_dir(tmp_path):
-    outdir = tmp_path / "sub"
-    outdir.mkdir()
-    generate(x, y, date, rowres, numrows, numsims, outdir)
-    assert outdir.is_dir()
-
-
 # Corrobora numero de archivos
 def test_create_weather(tmp_path):
-    outdir = tmp_path / "sub"
-    outdir.mkdir()
-    generate(x, y, date, rowres, numrows, numsims, outdir)
-    assert len(os.listdir(outdir)) == numsims
+    print(generate(x, y, date, rowres, numrows, numsims, tmp_path))
+    print(f"{tmp_path=}, {os.listdir(tmp_path)=}")
+    assert len(os.listdir(tmp_path)) == numsims, "Numero de archivos incorrecto"
 
 
 # Corrobra filas en el archivo
 def test_weather_lenght(tmp_path):
-    outdir = tmp_path / "sub"
-    outdir.mkdir()
-    generate(x, y, date, rowres, numrows, numsims, outdir)
-    for afile in outdir.iterdir():
-        assert len(afile.read_text().splitlines()) - 1 == numrows
+    print(generate(x, y, date, rowres, numrows, numsims, tmp_path))
+    print(f"{tmp_path=}, {os.listdir(tmp_path)=}")
+    for afile in tmp_path.iterdir():
+        assert len(afile.read_text().splitlines()) - 1 == numrows, "Numero de filas incorrecto"
+
+
+def test_invert_wind():
+    from numpy import linspace
+
+    from fire2a.meteo import barlo_sota, meteo_to_c2f
+
+    for a in linspace(1, 360, 12):
+        assert barlo_sota(a) - meteo_to_c2f(a) < 0.01, "Conversion incorrecta"
+    # for a in np.linspace(-360, 720, 12):
+    #     print(a, barlo_sota(a), meteo_to_c2f(a))
