37904e20b4551b8c5faf6159ea9e8cae5d3df03f
fdobad <29801096+fdobad@users.noreply.github.com>
2024-10-23 23:43:13 -0300

    documentation into module

--- src/fire2a/agglomerative_clustering.py
+++ src/fire2a/agglomerative_clustering.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python3
 """👋🌎 🌲🔥
+# Raster clustering
+## Usage
+### Overview
+1. Choose your raster files
+2. Configure nodata and scaling strategies in the `config.toml` file
+3. Choose "number of clusters" or "distance threshold" for the [Agglomerative](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html) clustering algorithm
 
-pyqgisdev # activate your qgis dev environment
-python -m fire2a.agglomerative_clustering -d 10.0 config.toml
+```bash
+source pyqgisdev/bin/activate # activate your qgis dev environment
+(qgis) $ python -m fire2a.agglomerative_clustering -d 10.0
+
+# windows💩
+C:\\PROGRA~1\\QGIS33~1.3\\bin\\python-qgis.bat -m fire2a.agglomerative_clustering -d 10.0
+
+# check help
+python agglomerative_clustering_pipeline.py -h
+```
+[how to: windows 💩 use qgis-python](https://github.com/fire2a/fire2a-lib/tree/main/qgis-launchers)
+
+### 1. Choose your raster files
+- Any [GDAL compatible](https://gdal.org/en/latest/drivers/raster/index.html) raster will be read
+- Place them all in the same directory where the script will be executed
+- "Quote them" if they have any non alphanumerical chars [a-zA-Z0-9]
+
+### 2. Preprocessing configuration
+See the `config.toml` file for example of the configuration of the preprocessing steps. The file is structured as follows:
+
+```toml
+["filename.tif"]
+no_data_strategy = "most_frequent"
+scaling_strategy = "onehot"
+fill_value = 0
+```
+
+1. __scaling_strategy__
+   - can be "standard", "robust", "onehot"
+   - default is "robust"
+   - [Standard](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html): (x-mean)/stddev
+   - [Robust](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.RobustScaler.html): same but droping the tails of the distribution
+   - [OneHot](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html): __for CATEGORICAL DATA__
+
+2. __no_data_strategy__
+   - can be "mean", "median", "most_frequent", "constant"
+   - default is "mean"
+   - categorical data should use "most_frequent" or "constant"
+   - "constant" will use the value in __fill_value__ (see below)
+   - [SimpleImputer](https://scikit-learn.org/stable/modules/generated/sklearn.impute.SimpleImputer.html)
+
+3. __fill_value__
+   - used when __no_data_strategy__ is "constant"
+   - default is 0
+   - [SimpleImputer](https://scikit-learn.org/stable/modules/generated/sklearn.impute.SimpleImputer.html)
+
+
+### 3. Clustering configuration
+1. __Agglomerative__ clustering algorithm is used. The following parameters are muttually exclusive:
+- `-n` or `--n_clusters`: The number of clusters to form as well as the number of centroids to generate.
+- `-d` or `--distance_threshold`: The linkage distance threshold above which, clusters will not be merged. When scaling start with 10.0 and downward (0.0 is compute the whole algorithm).
+
+For passing more parameters, see [here](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html)
+
 """
 import logging
 import sys
