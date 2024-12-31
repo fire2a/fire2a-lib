5f52f7de22deef85bdf097ba2f352870db4fc0b5
fdobad <8339628-fdobad@users.noreply.gitlab.com>
2024-12-31 11:20:10 -0300

    too complex almost working

--- src/fire2a/knapsack.py
+++ src/fire2a/knapsack.py
@@ -63,9 +63,10 @@ from fire2a.utils import fprint, read_toml
 
 logger = logging.getLogger(__name__)
 
-config_attrs = ["scaling_strategy", "value_weight", "capacity_sense", "capacity_ratio"]
+config_attrs = ["value_rescaling", "value_weight", "capacity_sense", "capacity_ratio"]
 config_types = [str, float, str, float]
-config_def = ["minmax", 1.0, "u", 0.0]
+config_def = ["", np.nan, "", np.nan]
+config_allowed = {"value_rescaling": ["minmax", "standard", "robust", "onehot"], "capacity_sense": ["<=", ">=", "leq", "geq", "ub", "lb", ""]}
 
 def check_shapes(data_list):
     """Check if all data arrays have the same shape and are 2D.
@@ -107,8 +108,8 @@ def pipelie(observations, config):
                 "scaler",
                 ColumnTransformer(
                     [
-                        (filename, scalers[cfg["scaling_strategy"]], [i])
-                        for i, (filename, cfg) in enumerate(config.items())
+                        (item['name'], scalers.get(item.get("value_rescaling")), [i])
+                        for i, item in enumerate(config) if item.get("value_rescaling")
                     ],
                     remainder="passthrough",
                 ),
@@ -168,6 +169,8 @@ def main(argv=None):
     """
 
     args = arg_parser(["-vvv", "tmpp9r9e05_.toml"])
+    args = arg_parser(["-vvv", "tmpaca3giia.toml"])
+     --authid EPSG:32718 --geotransform (720874.0, 100.0, 0.0, 5680572.0, 0.0, -100.0)
     """
     if argv is sys.argv:
         argv = sys.argv[1:]
@@ -183,23 +186,63 @@ def main(argv=None):
 
     # 2 LEE CONFIG
     config = read_toml(args.config_file)
-    # logger.debug(config)
+    a,b = list(config.keys()), list(config.values())
+    config = [ {'name': Path(a).name, 'filename': Path(a), **b} for a,b in zip(a,b) ]
+    for itm in config:
+        logger.debug(itm)
 
+    # 2.1 CHECK PAIRS
+    for itm in config:
+        if cr:=itm.get("capacity_ratio"):
+            if not (-1 < cr < 1):
+                logger.critical("Wrong value for capacity_ratio in %s, should be (-1,1)", itm)
+                sys.exit(1)
+            if "capacity_sense" not in itm:
+                logger.warning("capacity_ratio without capacity_sense for item: %s\n ASSUMING SENSE IS UPPER BOUND", itm["name"])
+                itm["capacity_sense"] = "ub"
+        elif "capacity_sense" in itm:
+            logger.critical("capacity_sense without capacity_ratio for item: %s", itm["name"])
+            sys.exit(1)
+        if vr:=itm.get("value_rescaling"):
+            if vr not in config_allowed["value_rescaling"]:
+                logger.critical("Wrong value for value_rescaling in %s", itm)
+                sys.exit(1)
+            if "value_weight" not in itm:
+                logger.warning("value_rescaling without value_weight for item: %s\n ASSUMING VALUE WEIGHT IS 1", itm["name"])
+                itm["value_weight"] = 1
+        elif "value_weight" in itm:
+            logger.warning("value_weight without value_rescaling for item: %s\n PASS THROUGH RESCALING", itm["name"])
     # 2.1 ADD DEFAULTS
-    for filename, file_config in config.items():
-        for cfg in config_attrs:
-            if cfg not in file_config:
-                config[filename][cfg] = config_def[config_attrs.index(cfg)]
-        logger.info("%s: %s", filename, config[filename])
-    logger.debug(config)
+    # for item in config:
+    #     for attr in config_attrs:
+    #         if attr not in item:
+    #             item[attr] = config_def[config_attrs.index(attr)]
+    #             print("adding default", attr, item[attr])
+    # for item in config:
+    #     #  val_resc & !val_weig ->  ?
+    #     #  !val_resc & val_weig ->  ?
+    #     #  cap_rati & !cap_sens ->  cap_sens = ub
+    #     if np.isfinite(item['capacity_ratio']) and item['capacity_sense'] == "":
+    #         item['capacity_sense'] = 'ub'
+    #         print("adding default", 'capacity_sense', item['capacity_sense'])
+    #     # !cap_rati &  cap_sens -> !cap_sens
+    #     if item['capacity_ratio'] is np.nan and item['capacity_sense'] != "":
+    #         item['capacity_sense'] = ""
+    #         print("adding default", 'capacity_sense', item['capacity_sense'])
+    # for item in config:
+    #     for key in item:
+    #         if key in config_allowed:
+    #             assert item[key] in config_allowed[key], f"Wrong value for {key} in {item}"
+    #     logger.info("%s", item)
+    # logger.debug(config)
 
     # 3. LEE DATA
     from fire2a.raster import read_raster
 
     data_list = []
-    for filename, cfg in config.items():
-        data, info = read_raster(filename)
-        cfg.update(info)
+    for item in config:
+        data, info = read_raster(str(item["filename"]))
+        item.update(info)
         data_list += [data]
 
     # 4. VALIDAR 2d todos mismo shape
@@ -209,8 +252,9 @@ def main(argv=None):
     all_observations = np.column_stack([data.ravel() for data in data_list])
 
     # 6. if all rasters are nodata then mask out 
-    nodatas = [ cfg["NoDataValue"] for cfg in config.values() ]
+    nodatas = [ item["NoDataValue"] for item in config ]
     nodata_mask = np.all(all_observations == nodatas, axis=1)
+    logger.info("All rasters NoData: %s pixels", nodata_mask.sum())
     observations = all_observations[~nodata_mask]
 
     # 7. nodata -> 0 
@@ -220,38 +264,70 @@ def main(argv=None):
     # scaling
     # 8. PIPELINE
     scaled, pipe, feat_names = pipelie(observations, config)
+    assert observations.shape[0] == scaled.shape[0]
+    assert observations.shape[1] <= scaled.shape[1]
 
     # weights
-    value_weights = []
+    values_weights = []
     for name in feat_names:
-        for filename, cfg in config.items():
-            if name.startswith(filename):
-                value_weights += [cfg["value_weight"]]
+        for item in config:
+            if name.startswith(item["name"]):
+                values_weights += [item["value_weight"]]
 
     # capacities
-    cap = []
-    for i,(filename, cfg) in enumerate(config.items()):
-        if cr:=cfg.get(["capacity_ratio"):
-            cap += [observations[:,i].sum() * cfg["capacity_ratio"]]
-        else:
-            cap += [None]
+    # ["<=", "leq", "ub"]
+    # [">=", "geq", "lb"]
+    cap_cfg= [ {"idx": i,
+            "name": item["filename"].stem,
+             "cap": observations[:,i].sum() * item["capacity_ratio"],
+             "sense": item["capacity_sense"]}
+              for i,item in enumerate(config) if item["capacity_ratio"] is not np.nan
+           ]
+    cap_data = observations[:,[itm["idx"] for itm in cap_cfg]] 
 
-    # 9. KnapSack
+    # 9. PYOMO MODEL & SOLVE
     from pyomo import environ as pyo
 
     # m model
     m = pyo.ConcreteModel("MultiObjectiveKnapsack")
     # sets
-    m.N = pyo.RangeSet(0, observations.shape[0] - 1)
-    m.V = pyo.RangeSet(0, observations.shape[1] - 1)
+    m.V = pyo.RangeSet(0, scaled.shape[1] - 1)
+    scaled_n, scaled_v = scaled.nonzero()
+    m.NV = pyo.Set( initialize=[(n,v) for n,v in zip(scaled_n, scaled_v)] )
+    m.W = pyo.RangeSet(0, len(cap_cfg) - 1)
+    cap_data_n, cap_data_v = cap_data.nonzero()
+    m.NW = pyo.Set( initialize=[(n,w) for n,w in zip(cap_data_n, cap_data_v)] )
+    both_nv_nw = list(set(scaled_n) | set(cap_data_n))
+    both_nv_nw.sort()
+    m.N = pyo.Set( initialize=both_nv_nw)
     # parameters
-    m.v = pyo.Param(m.V,m.N, initialize=lambda v, n: scaled[n,v])
+    m.scaled_values = pyo.Param(m.NV, initialize=lambda m, n, v: scaled[n,v])
+    m.values_weight = pyo.Param(m.V, initialize=values_weights)
+    m.total_capacity = pyo.Param(m.W, initialize=[itm["cap"] for itm in cap_cfg])
+    m.capacity_weight = pyo.Param(m.NW, initialize=lambda m, n, w: cap_data[n,w])
     # variables
     m.X = pyo.Var(m.N, within=pyo.Binary)
     # constraints
-    m.capacity = pyo.Constraint(rule=lambda m: pyo.sum_product(m.X, m.w, index=m.N) <= m.C)
+    def capacity_rule(m, w):
+        if cap_cfg[w]["sense"] in ["<=", "leq", "ub"]:
+            return sum(m.X[n] * m.capacity_weight[n,w] for n,ww in m.NW if ww==w) <= m.total_capacity[w]
+        elif cap_cfg[w]["sense"] in [">=", "geq", "lb"]:
+            return sum(m.X[n] * m.capacity_weight[n,w] for n,ww in m.NW if ww==w) >= m.total_capacity[w]
+        else:
+            logger.critical("Wrong sense for capacity constraint")
+            return pyo.Constraint.Skip
+    m.capacity = pyo.Constraint(m.W, rule=capacity_rule)
+
     # objective
     m.obj = pyo.Objective(
-        expr=pyo.sum_product(m.value_weight[v] * pyo.sum_product(m.X, m.v[v], index=m.N), index=m.N)
+        expr=sum(m.values_weight[v] * sum(m.X[n]* m.scaled_values[n,v] for n,vv in m.NV if vv==v) for v in m.V),
         sense=pyo.maximize,
     )
+
+    from pyomo.opt import SolverFactory
+    opt = SolverFactory("cplex")
+    results = opt.solve(m, tee=True)
+    soln = np.array([pyo.value(m.X[i], exception=False) for i in m.X], dtype=float)
+    slacks = m.capacity[:].slack()
+    [ print(f"{itm['name']} cap:{itm['cap']} sense:{itm['sense']} slack:{slacks[i]}") for i,itm in enumerate(cap_cfg) ]
+    m.obj()
