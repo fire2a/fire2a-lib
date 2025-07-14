ca31ecc858b7c28a7271b258f1302cf0ff243b8d
Matias <matiasvilches@Matiass-MacBook-Air.local>
2025-07-14 09:16:10 -0400

    itrend code finish

--- src/new_pp/itrend.py
+++ src/new_pp/itrend.py
@@ -8,22 +8,11 @@ import pandas as pd
 from sim_temp import simulate_season_hyperparameters
 
 sys.path.append('/Users/matiasvilches/Documents/F2A/source/tools/post-processing/')
-from operations_raster import read_asc
+from operations_raster import read_asc,average_asc_files
 from operations_raster import write_asc
 from operations_msg import get_graph
 from calculator_bp import bp_calculation
 
-
-"""
-# 1. SIMULAR INCENDIOS INDIVIDUALES
-# 2. ACTUALIZAR MAPA DE COMBUSTIBLES CON LA SUPERFICIE QUEMADA
-# 3. SE SIMULA HASTA CUMPLIR ALGUN CRITERIO DE PARADA (PORCENTAJE DE AREA QUEMADA, NUMERO DE INCENDIOS SUPERIORES A 5HA)
-# 4. SE OBTIENEN LOS MAPAS DE FLAME LENGTH, FIRELINE INTENSITY, ETC. DE CADA INCENDIO
-# 5. SE SIMULAN LAS TEMPORADAS NECESARIAS
-# 6. SE PROMEDIAN LOS RESULTADOS PARA TODAS LAS TEMPORADAS
-# 7. SE OBTIENEN LOS MAPAS DE RIESGO DE INCENDIO
-# 8. SE OBTIENEN LOS MAPAS DE RIESGO DE INCENDIO CON LA SUPERFICIE QUEMADA PARA LAS CELDAS QUE SUPEREN EL UMBRAL DE RIESGO
-"""
 
 def create_dir(directory):
     """
@@ -65,7 +54,7 @@ def c2f_simulation(
         '--seed',str(seed), #Random seed
         '--output-messages',
         '--weather random',
-        '--nweathers 50',
+        '--nweathers 30',
         '--statistics'  # Enable statistics output
     ])
     subp.call(command, shell=True, stdout=subp.DEVNULL)  # Run the C2F simulation command
@@ -131,9 +120,14 @@ def stop_criteria(
 
 if __name__ == '__main__':
     
-    temporadas = 1
+    temporadas = 1000
     forest = 'Biobio'
     forest_path = f'/Users/matiasvilches/Documents/F2A/ITREND/forest/{forest}/fuels.asc'
+    original_fuels = f'/Users/matiasvilches/Documents/F2A/ITREND/forest/{forest}/fuels.asc'
+    fuel_wip_path = f'/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}_wip/fuels.asc'
+    total_incendios = []
+
+    #"""
     
     for t in range(1, temporadas + 1):
         print(10*'-',f'Starting season {t}',10*'-')
@@ -141,17 +135,15 @@ if __name__ == '__main__':
         scar_sizes = []
         stop = False
         n_simulacion = 1
-        original_fuels = f'/Users/matiasvilches/Documents/F2A/ITREND/forest/{forest}/fuels.asc'
-        fuel_wip_path = f'/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}_wip/fuels.asc'
         forest_path = f'/Users/matiasvilches/Documents/F2A/ITREND/forest/{forest}/'
         path = '/Users/matiasvilches/Documents/F2A/ITREND/data/BD_Incendios.csv'
-        total_incendios = []
 
         # Reset the working fuel map at the start of each season
         recover_fuels(original_fuels, fuel_wip_path)
         
         incendios, area = simulate_season_hyperparameters(path)
-        print(f'Incendios: {incendios}, Area: {area}')
+        area = area*8
+        #print(f'Incendios: {incendios}, Area: {area}')
         
         while stop == False:
             
@@ -174,10 +166,11 @@ if __name__ == '__main__':
                 continue
             scar_sizes.append(scar_size)
             season_size = sum(scar_sizes)
-            if season_size >= area*10:
+            if season_size >= area:
                 #print(f'Season size: {season_size}, Area: {area}')
-                stop = True
                 total_incendios.append(n_simulacion)
+                print(f'Season size: {season_size}, Area: {area}, Total Incendios: {total_incendios}')
+                stop = True
                 continue
             else:
                 print(f'Season size: {season_size}, Area: {area}')
@@ -186,13 +179,13 @@ if __name__ == '__main__':
             # 3. Actualizar mapa de combustibles con la superficie quemada
             update_fuelmap(fuel_wip_path, burned_cells)
             n_simulacion +=1
-            forest_path = f'/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}_wip/'
             #if n_simulacion == incendios_temporada:
                 #recover_fuels(original_fuels,fuel_wip_path)
                 #stop = True
-           
+     #"""       
     # 4. Calculate excedence ratio maps
     # for every season and simulation, calculate the times a cell exceded 3 meters of flame lenght
+    #total_incendios = [20,14,2,26,1,10,31,16,15,22]
     season_excedence = {}
     season_messages = {}
     for t in range(1, temporadas + 1):
@@ -202,6 +195,7 @@ if __name__ == '__main__':
             flame_length_map = f'{output_path}/Statistics/statisticsPerSim.csv'
             flame_length = pd.read_csv(flame_length_map, sep=',')
             flame_length = flame_length.loc[0,'surfaceFlameLengthMean']
+            print(f'Flame length for simulation {n_simulacion} in season {t}: {flame_length}')
 
             if flame_length < 3:
                 continue
@@ -224,17 +218,27 @@ if __name__ == '__main__':
     for t in range(1, temporadas + 1):
         bp_file_output = f'{bp_output}/bp_t{t}.asc'
         pickle_path = f'/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}/t{t}/Pickles/'
-        bp_calculation(forest_path, season_messages[t], pickle_path, bp_file_output, len(season_messages[t]), ncores)
+        try:
+            bp_calculation(original_fuels, season_messages[t], pickle_path, bp_file_output, len(season_messages[t]), ncores)
+        except KeyError as e:
+            print(f"KeyError: {e}. No messages found for season {t}. Skipping Burn Probability calculation.")
+            continue
+    
 
     # 6. Estimate the excedence ratio map using burn probability
+    # if there is no burn probability map for specific season, skip it
     for t in range(1, temporadas + 1):
         excedence_by_cell = season_excedence[t]
         #print(f'Excedence by cell for season {t}: {excedence_by_cell}')
         bp_file_output = f'{bp_output}/bp_t{t}.asc'
-        with rasterio.open(bp_file_output) as src:
-            profile = src.profile
-            data = src.read(1)
-            nrows, ncols = data.shape
+        try:
+            with rasterio.open(bp_file_output) as src:
+                profile = src.profile
+                data = src.read(1)
+                nrows, ncols = data.shape
+        except:
+            print(f"Error: Burn Probability map for season {t} not found or inaccessible. Skipping Excedance Ratio calculation.")
+            continue
         
         #multiply the burn probability by the number of times a cell exceeded 3 meters of flame length
         for i in range(nrows):
@@ -244,8 +248,16 @@ if __name__ == '__main__':
                     cell_id = i * ncols + j + 1  # Uncomment if using
                     data[i, j] *= excedence_by_cell[cell_id]
         #write the excedence ratio map
-        excedence_ratio_output = f'{bp_output}/excedance_t{t}.asc'
+        excedence_folder_output = f"/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}/excedance_maps/"
+        create_dir(excedence_folder_output)
+        excedence_ratio_output = f"{excedence_folder_output}excedance_t{t}.asc"
         with rasterio.open(excedence_ratio_output, 'w', **profile) as dst:
             dst.write(data, 1)
 
         print("Excedance ratio maps calculated for season", t)
+
+    excedence_folder_output = f"/Users/matiasvilches/Documents/F2A/ITREND/results/{forest}/excedance_maps/"
+    input = excedence_folder_output
+    output = f"{excedence_folder_output}average_excedance.asc"
+    average_asc_files(input, output)
+    
